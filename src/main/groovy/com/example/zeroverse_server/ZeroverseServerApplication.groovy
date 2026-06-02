75990576b19b5dbe18482be473d0534e5a51b361
박영균 <pyk09667@gmail.com>
2026-06-02 16:18:12 +0900

    프로젝트 초기 설정

---
+++ src/main/groovy/com/example/zeroverse_server/ZeroverseServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.zeroverse_server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ZeroverseServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ZeroverseServerApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/zeroverse_server/ZeroverseServerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.zeroverse_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZeroverseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
